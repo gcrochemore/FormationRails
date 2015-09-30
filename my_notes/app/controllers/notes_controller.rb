5dffdc838ab0cdd902eb95cc793ec80ae3d9d82f
gcrochemore <guillaume.crochemore@chabe-limousines.com>
2015-09-30 16:53:28 +0200

    Création de la BDD

--- my_notes/app/controllers/notes_controller.rb
+++ my_notes/app/controllers/notes_controller.rb
@@ -1,6 +1,9 @@
 class NotesController < ApplicationController
 	def index
 		@messages = params[:messages].present? ? params[:messages] : ["World"]
+		@nb_zones = params[:nb_zones].present? ? params[:nb_zones].to_i : 3
+		@nb_zones = params[:ajouter].present? ? @nb_zones+1 : @nb_zones
+		@nb_zones = params[:supprimer].present? ? @nb_zones-1 : @nb_zones
 		ap params
 	end
 end
